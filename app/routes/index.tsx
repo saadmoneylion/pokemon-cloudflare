1451a37e73757050f56d789a5342f4bb4d9c4441
saadmoneylion <sullah@moneylion.com>
2023-02-07 12:52:21 +0800

    Added a New Page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,7 @@
 import { json } from "@remix-run/server-runtime";
-import { Link, useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData, } from "@remix-run/react";
 import { getPokemons } from "~/models/pokemon.server";
+import translation from "../translations/en";
 
 type LoaderData = {
   data: Awaited<ReturnType<typeof getPokemons>>;
@@ -17,7 +18,15 @@ export default function Index() {
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">
-        Which Pokémon do you want to catch?</h1>
+        Which Pokémon do you want to catch?
+      </h1>
+      <div className="flex w-2/4 h-10 rounded-full justify-center text-xl m-auto bg-teal-500 cursor-pointer">
+        <Link
+          className="flex justify-center text-base mt-2 text-black cursor-pointer"
+          to={"/signup"}>
+          {translation.login.cta_sign_up_now}
+        </Link>
+      </div>
       <ul className='mx-auto text-center'>
         {data.map((pokemon: any) => (
           <li key={pokemon.name}>
